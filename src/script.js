69a5d7da8b63b6e9d9b837c2e3a9b34e1fcec90a
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-16 16:54:39 -0500

    deploy

--- src/script.js
+++ src/script.js
@@ -1,40 +1,91 @@
-const world = document.querySelector(".world");
-//sprite.style.margin = `${y}px ${x}px`;
+const input = document.querySelector(".input")
+const button = document.querySelector(".button")
+const world = document.querySelector(".world")
 
-const input = document.querySelector(".input");
-const button = document.querySelector(".button");
-
-function peopleLogin(){
-  const sprite = document.createElement("img")
-  sprite.src = "./src/img/sprite.png"
-  sprite.className = "sprite";
-  document.body.appendChild(sprite)
+async function login(){
+  fetch(`https://mmorpgteste.herokuapp.com/create`,
+  {
+    method: "post",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({name: input.value}),
+  })
+  localStorage.setItem("nick", input.value);
+  await player()
 }
 
-button.addEventListener("click", peopleLogin)
+button.addEventListener("click", async function(){
+  await login()
+})
 
-function render(){
-    world.innerHTML = "";
+async function geolocationCharacter(){
+  const geolocation = fetch(`https://mmorpgteste.herokuapp.com`,{method: 'GET'})
+      return (await geolocation).json()
 }
 
-render()
+async function player(){
+  world.innerHTML = "";
+  const dataBase = await geolocationCharacter()
+  dataBase.forEach(player => {
+    const {name, x, y} = player
+    const div = document.createElement("div")
+    div.className = "player";
+    div.style.margin = `${y}px ${x}px`;
+    const namePlayer = document.createElement("p")
+    namePlayer.innerText = name;
+    const sprite = document.createElement("img")
+    sprite.src = "./src/img/sprite.png"
+    div.appendChild(namePlayer)
+    div.appendChild(sprite)
+    world.appendChild(div)
+  });
+}
 
-function motion(evt){
+async function updatePlayer(valueX, valueY){
+
+  const nick = localStorage.getItem("nick");
+
+  fetch(`https://mmorpgteste.herokuapp.com/update`,
+  {
+    method: "put",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({name: nick, x: valueX, y:valueY}),
+  })
+  await player()
+}
+
+async function infoPlayer(){
+  const nick = localStorage.getItem("nick");
+  const index = await fetch(`https://mmorpgteste.herokuapp.com/${nick}`,{method: "get"})
+  return index.json()
+} 
+
+async function motion(evt){
+
+  const info = await infoPlayer()
+  console.log(info)
+  const {x, y} = info
+
+  let valueX = x
+  let valueY = y
+
    if(evt.key === "ArrowLeft"){
-     x -= 10;
+    valueX -= 10;
    }
    if(evt.key === "ArrowRight"){
-     x += 10;
+    valueX += 10;
    }
    if(evt.key === "ArrowUp"){
-     y -= 10;
+    valueY -= 10;
    }
    if(evt.key === "ArrowDown"){
-     y += 10;
+    valueY += 10;
    }
-   render();
+
+   await updatePlayer(valueX, valueY)
 }
 
-document.body.addEventListener("keydown", function(evt){
-    motion(evt);
+document.body.addEventListener("keydown", async function(evt){
+    await motion(evt);
 });
+
+setInterval(async ()=>{await player()}, 1000)
